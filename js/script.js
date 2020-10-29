8cff370e236ce257d9289f2be458887a8ba36537
Salvatore Borgia <sa.borgia3@gmail.com>
2020-10-29 12:40:51 +0100

    aggiunta condizioni limite

--- js/script.js
+++ js/script.js
@@ -9,8 +9,11 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
 //empty array to fill on
 var bombs = [];
 var userNumbers = [];
+var userPromptNumbers = [];
 var numberBombMatch = false;
 var numberInvalid = false;
+var doubleNumber = false;
+
 //pushing random numbers (1 to 100) through a loop 16 times
 for (var i = 0; i < 16; i++) {
   bombs.push(Math.round(Math.random() * 100) + 1);
@@ -24,35 +27,47 @@ console.log("Tot numeri bomba: ", bombs.length);
 for (var i = 0; i < 84; i++) {
   var askNumber = parseInt(prompt("Inserisci numero da 1 a 100"));
   console.log(askNumber);
-  if (askNumber <= 0 || askNumber > 100 || isNaN(askNumber)) {
+  //check if the number is not out of range (1 to 100) and is a number.
+  if (askNumber < 1 || askNumber > 100 || isNaN(askNumber)) {
     console.log(askNumber, " non è un numero valido");
     alert("Non è un numero valido, Fine partita!!!");
-    // numberBombMatch = true; //variabile che decreta fine gioco come se beccasse un numero bomba
-    numberInvalid = true;
+    numberInvalid = true; //variabile che decreta fine gioco come se beccasse un numero bomba
+  } else {
+    userPromptNumbers.push(askNumber); //array to check if input number is unique (no double items)
+    userNumbers.pop();
   }
+  //loop that scan over array in order to find double items in same array
+  for (var x = 0; x < userNumbers.length; x++) {
+    if (askNumber === userPromptNumbers[x]) {
+      console.log("Hai messo un numero già scelto precedentemente");
+      userNumbers.pop();
+    }
+  }
+
+  console.log("Numeri che vanno nell'array dei doppi: ", userPromptNumbers);
 
   //loop to determine if user number is ==== to one of array.bombs numbers(bomb)
   for (var j = 0; j < bombs.length; j++) {
-    //finchè j rimane minore del numero di item nell'array bombs
     if (askNumber === bombs[j]) {
-      //se il numero utente è uguale a uno dei valori contenuti nell'array bombs
       numberBombMatch = true;
     }
   }
-  //checking if user choose a bomb number //I'll have to move this part  inside the nested loops
+  //checking if user choose a bomb number
   if (numberBombMatch === true) {
-    console.log("Hai preso la bomba!! BOOOM");
+    console.log("Hai preso la bomba!! BOOOM"); //Game Over
+    console.log("Punteggio utente: ", userNumbers.length);
     break;
   } else if (numberInvalid === true) {
     console.log("Hai usato un numero invalido, hai perso!!");
     break;
-    //if it is not, adding number to user score variable, then asking for another input number again and again (max 84 times)
+    //else adding number to user score variable
   } else {
-    userNumbers.push(askNumber); //altrimenti se non è uguale pusho l'elemento per tenere lo score utente
+    userNumbers.push(askNumber);
   }
 }
 
 console.log("Numeri utente: ", userNumbers);
+console.log("Numeri Possibili doppi ", userPromptNumbers);
 console.log("Punteggio utente: ", userNumbers.length);
 
 //check if number input is on array bombs and if the number is not out of range (1 to 10).
