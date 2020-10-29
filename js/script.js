122f34f8300f58a1f1d22ed6b91dd2ceac1b688b
Salvatore Borgia <sa.borgia3@gmail.com>
2020-10-29 17:05:56 +0100

    esercizio refactoring da capo

--- js/script.js
+++ js/script.js
@@ -9,23 +9,33 @@ Al termine della partita il software deve comunicare il punteggio, cioè il nume
 //empty array to fill on
 var bombs = [];
 var userNumbers = [];
-var userPromptNumbers = [];
 var numberBombMatch = false;
 var numberInvalid = false;
-var doubleNumber = false;
 
 //pushing random numbers (1 to 100) through a loop 16 times
-for (var i = 0; i < 16; i++) {
-  bombs.push(Math.round(Math.random() * 100) + 1);
+while (bombs.length < 16) {
+  var rndmNumb = Math.round(Math.random() * 100) + 1;
+  if (bombs.includes(rndmNumb) === false) bombs.push(rndmNumb);
 }
 
 console.log("Numeri bomba del pc ", bombs);
 console.log("Tot numeri bomba: ", bombs.length);
 
 //asking input numbers between 1 to 100 (84 times max) Loop max 84 times
-
-for (var i = 0; i < 84; i++) {
+var i = 0;
+var mintr = false;
+while (userNumbers.length < 84 && mintr === false) {
   var askNumber = parseInt(prompt("Inserisci numero da 1 a 100"));
+  userNumbers.push(askNumber);
+  console.log(askNumber);
+  console.log(userNumbers);
+  if (bombs.includes(askNumber) === true) {
+    console.log("Il numero " + askNumber + " è una Bomba");
+    mintr = true;
+  }
+}
+
+/*   var askNumber = parseInt(prompt("Inserisci numero da 1 a 100"));
   console.log(askNumber);
   //check if the number is not out of range (1 to 100) and is a number.
   if (askNumber < 1 || askNumber > 100 || isNaN(askNumber)) {
@@ -34,27 +44,29 @@ for (var i = 0; i < 84; i++) {
     numberInvalid = true; //variabile che decreta fine gioco come se beccasse un numero bomba
   } else {
     userPromptNumbers.push(askNumber); //array to check if input number is unique (no double items)
-    userNumbers.pop();
   }
   //loop that scan over array in order to find double items in same array
   for (var x = 0; x < userNumbers.length; x++) {
     if (askNumber === userPromptNumbers[x]) {
       console.log("Hai messo un numero già scelto precedentemente");
-      userNumbers.pop();
+      // var risultatoFinale = Array.from(new Set(userNumbers));
     }
   }
+
+  // Loop through array values to remove duplicate numbers
 
   console.log("Numeri che vanno nell'array dei doppi: ", userPromptNumbers);
+  console.log("Numeri utente ", userNumbers);
 
   //loop to determine if user number is ==== to one of array.bombs numbers(bomb)
-  for (var j = 0; j < bombs.length; j++) {
+/*   for (var j = 0; j < bombs.length; j++) {
     if (askNumber === bombs[j]) {
       numberBombMatch = true;
-    }
-  }
+    } */
+/*   }
   //checking if user choose a bomb number
   if (numberBombMatch === true) {
-    console.log("Hai preso la bomba!! BOOOM"); //Game Over
+    console.log("Il numero", askNumber, " è la bomba!!! BOOOM"); //Game Over
     console.log("Punteggio utente: ", userNumbers.length);
     break;
   } else if (numberInvalid === true) {
@@ -65,15 +77,7 @@ for (var i = 0; i < 84; i++) {
     userNumbers.push(askNumber);
   }
 }
-
-console.log("Numeri utente: ", userNumbers);
+ */
+/* console.log("Numeri utente: ", userNumbers);
 console.log("Numeri Possibili doppi ", userPromptNumbers);
-console.log("Punteggio utente: ", userNumbers.length);
-
-//check if number input is on array bombs and if the number is not out of range (1 to 10).
-// if all said above is true then end game.
-
-//if it is not, adding number to user score variable, then asking for another input number again and again (max 84 times)
-
-//Output
-//after end game print score (score = how many times user has choose a valid number )
+console.log("Punteggio utente: ", userNumbers.length); */
